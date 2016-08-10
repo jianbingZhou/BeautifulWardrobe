ec4acfc739f51383cba591b2ceef3b8122a2e40d
jianbingZhou <986278051@qq.com>
2016-08-10 14:47:43 +0800

    Initial Commit

---
+++ BeautifulWardrobe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BeautifulWardrobe
+//
+//  Created by Terra MacBook on 16/8/10.
+//  Copyright © 2016年 Jianbing Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
